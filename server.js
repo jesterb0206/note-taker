0389addd44ab0315da98f20936000e2dcd4ce62d
jesterb0206 <jesterb@seattleu.edu>
2022-11-07 15:30:04 -0800

    HTML Routes

--- server.js
+++ server.js
@@ -20,3 +20,17 @@ app.use(express.urlencoded({ extended: true }));
 // Local Port Listening
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+
+// HTML Routes
+
+// Returns the index.html file
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// Returns the notes.html file
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
